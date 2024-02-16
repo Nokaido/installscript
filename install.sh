89c7d9fcab9dbb8cbb6772b73666991092d18398
Nokaido <kolbandreasmartin@gmail.com>
2024-02-16 08:06:30 +0100

    fix

--- install.sh
+++ install.sh
@@ -11,11 +11,12 @@ sudo apt install curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
-sudo apt install brave-browser
+sudo apt install brave-browser -y
 sudo apt-get install code
 code --install-extension ms-toolsai.jupyter
 code --install-extension ms-python.vscode-pylance
-code --install-extension ms-python.python
+code --install-extension ms-python.pytho
+
 code --install-extension ms-python.debugpy
 python3 -m venv /home/nokaido/workspace/camenv
 source /home/nokaido/workspace/camenv/bin/activate
